5c42bf2826f6e2fb02ac9d5016a9fcc29d184a63
包振栋 <mot-baozhendong@zhaopin.com.cn>
2017-09-12 14:32:23 +0800

    新增修改.net core

---
+++ CoreConseleApp/WebAppCore2/Controllers/FistController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppCore2.Controllers
+{
+    public class FistController : Controller
+    {
+        public IActionResult FistIndex()
+        {
+            return View();
+        }
+    }
+}
